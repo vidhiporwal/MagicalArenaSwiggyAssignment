9cfad7de29913456b072761b59e4f623da798293
vidhiporwal <vidhiporwal2401@gmail.com>
2024-06-21 19:23:00 +0530

    Initial commit

---
+++ src/main/java/com/lld/magicalarena/MagicalArenaGameApplication.java
@@ -0,0 +1,13 @@
+package com.lld.magicalarena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicalArenaGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicalArenaGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lld/magicalarena/MagicalArenaGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lld.magicalarena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicalArenaGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
