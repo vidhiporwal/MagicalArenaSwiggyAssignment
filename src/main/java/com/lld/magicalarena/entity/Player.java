aa128c065ea55490eb77ec9356d604d776bd8c94
vidhiporwal <vidhiporwal2401@gmail.com>
2024-06-21 19:31:37 +0530

    creating entity class

---
+++ src/main/java/com/lld/magicalarena/entity/Player.java
@@ -0,0 +1,17 @@
+package com.lld.magicalarena.entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+public class Player {
+
+	private int health;
+    private int strength;
+    private int attack;
+}
